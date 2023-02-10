8de43334a2c360d2b0aac54ebaed0edbba63e468
M4Y50N <pethersonreis@gmail.com>
2023-02-09 21:36:34 -0300

    update sql

--- databasesprint3.sql
+++ databasesprint3.sql
@@ -1,38 +1,79 @@
 CREATE DATABASE databasesprint3;
 
+CREATE TYPE os AS ENUM ('windows', 'linux', 'macos');
+
+CREATE TABLE IF NOT EXISTS developer (
+    "developerID" SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS developer_infos (
-    id SERIAL;
-    "developerSince" DATE NOT NULL;
-    "preferedOS" VARCHAR NOT NULL;
+    "developerInfoID" SERIAL PRIMARY KEY,
+    "developerSince" DATE NOT NULL,
+    "preferredOS" os,
+    "developerID" INTEGER UNIQUE,
+    FOREIGN KEY ("developerID") REFERENCES developer("developerID") ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS developers (
-    id SERIAL;
-    name VARCHAR(50) NOT NULL;
-    email VARCHAR(50) NOT NULL;
-    "developerInfoID" INT REFERENCES developer_infos("id")
-    "developerSince" DATE REFERENCES developer_infos("developerSince");
-    "preferedOS" VARCHAR REFERENCES developer_infos("preferedOS");
+CREATE TABLE IF NOT EXISTS project (
+    "projectID" SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT,
+    "estimatedTime" VARCHAR(20) NOT NULL,
+    repository VARCHAR(120) NOT NULL,
+    "startDate" DATE NOT NULL,
+    "endDate" DATE,
+    "developerID" INTEGER NOT NULL,
+    FOREIGN KEY ("developerID") REFERENCES developer("developerID") ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS projets (
-    id SERIAL;
-    name VARCHAR(50) NOT NULL;
-    description TEXT NOT NULL;
-    "estimatedTime" VARCHAR(20) NOT NULL;
-    repository VARCHAR(120) NOT NULL;
-    "startDate" DATE NOT NULL;
-    "endDate" DATE;
+INSERT INTO 
+	project(name, description, "estimatedTime", repository, "startDate", "developerID")
+VALUES
+	('Projeto 1', 'projeto full stack', '2 dias', 'url.com.br', '02/13/2023', 1);	
+	
+
+CREATE TABLE IF NOT EXISTS technology (
+    "techID" SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS technologies (
-    id SERIAL;
-    name VARCHAR(30) NOT NULL;
-    "techList" ARRAY[];
+CREATE TABLE IF NOT EXISTS project_technology (
+    "projectTechID" SERIAL PRIMARY KEY,
+    "addedIn" DATE NOT NULL,
+    "projectID" INTEGER,
+    FOREIGN KEY ("projectID") REFERENCES project("projectID") ON DELETE CASCADE,
+    "techID" INTEGER UNIQUE,
+    FOREIGN KEY ("techID") REFERENCES technology("techID") ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS projects_technologies (
-    id SERIAL;
-    "addedIn" DATE NOT NULL;
-);
 
+INSERT INTO 
+	project_technology ("addedIn", "projectID", "techID") 
+VALUES 
+	('02/23/2023', 1, 2);
+
+INSERT INTO 
+		technology(name)
+VALUES 
+		('javascript'),
+		('python'),
+		('react'),
+		('expressjs'),
+		('html'),
+		('css'),
+		('django'),
+		('postgresql'),
+		('mongodb');
+
+
+INSERT INTO 
+	developer(name, email) 
+VALUES 
+	('Fabio', 'fabio.jr@kenzie.com.br');
+
+INSERT INTO 
+	developer_infos("developerSince", "preferredOS", "developerID") 
+VALUES 
+	('01/10/2021', 'windows', 1)
